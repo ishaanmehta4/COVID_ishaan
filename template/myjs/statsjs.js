0d8ca0a786be922eb8bed1753218773acc2de14a
Ishaan Mehta <ishaanmehta4@gmail.com>
2020-04-12 15:39:13 +0530

    updated stats

--- template/myjs/statsjs.js
+++ template/myjs/statsjs.js
@@ -1,39 +1,43 @@
 'use strict';
-            //var c=prompt();
-            (async () => {
 
-            var searchin="f";
+(async () => {
 
-            var response = await fetch('https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search?search='+searchin);
-            
-            var text = await response.text(); // read response body as text
-            
-            var y=JSON.parse(text);
-            var p="<thead><tr><th>Country</th><th>Total cases</th><th>Currently infected</th><th>Total recovered</th><th>Deaths</th></tr></thead><tfoot><tr><th>Country</th><th>Total cases</th><th>Currently infected</th><th>Total recovered</th><th>Deaths</th></tr></tfoot><tbody>";
+  var getQueryString = function (field, url) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+  };
+  var searchin = getQueryString('search');
+
+  document.getElementById("thead").innerHTML='Search results for "'+searchin+'"';
 
 
 
-            document.getElementById("thead").innerHTML=y.data.paginationMeta.currentPageSize;
-            for(var i=0;i<y.data.paginationMeta.currentPageSize;i++)
-            {
-                var m=y.data.rows[i];
-                p+="<tr>"+"<td>"+m.country+"</td>";
-                p+="<td>"+m.total_cases+"</td>";
-                p+="<td>"+m.active_cases+"</td>";
-                p+="<td>"+m.total_recovered+"</td>";
-                p+="<td>"+m.total_deaths+"</td></tr>";
-            }
-            p+="</tbody";
-            document.getElementById("dataTable").innerHTML=p;
-                
+  var response = await fetch('https://corona-virus-stats.herokuapp.com/api/v1/cases/countries-search?search=' + searchin);
 
-            
-          //  document.innerHTML=x;
-            // document.getElementsByClassName("ininf")[0].innerHTML=(y.data.rows.active_cases);
-            // document.getElementsByClassName("inrec")[0].innerHTML=(y.data.rows.total_recovered);
-            // document.getElementsByClassName("indeath")[0].innerHTML=(y.data.rows.total_deaths);
-            // document.getElementsByClassName("wwinf")[0].innerHTML=(yy.data.currently_infected);
-            // document.getElementsByClassName("wwrec")[0].innerHTML=(yy.data.recovery_cases);
-            // document.getElementsByClassName("wwdeath")[0].innerHTML=(yy.data.death_cases);
+  var text = await response.text(); // read response body as text
 
-            })()
+  var y = JSON.parse(text);
+  var p = "<thead><tr><th>Country</th><th>Total cases</th><th>Currently infected</th><th>Total recovered</th><th>Deaths</th></tr></thead><tfoot><tr><th>Country</th><th>Total cases</th><th>Currently infected</th><th>Total recovered</th><th>Deaths</th></tr></tfoot><tbody>";
+
+
+
+  for (var i = 0; i < y.data.paginationMeta.currentPageSize; i++) {
+    var m = y.data.rows[i];
+    p += "<tr>" + "<td>" + m.country + "</td>";
+    p += "<td>" + m.total_cases + "</td>";
+    p += "<td>" + m.active_cases + "</td>";
+    p += "<td>" + m.total_recovered + "</td>";
+    p += "<td>" + m.total_deaths + "</td></tr>";
+  }
+  p += "</tbody";
+  document.getElementById("dataTable").innerHTML = p;
+
+
+
+
+
+
+
+})()
